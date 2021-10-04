5ded64dafd440c3d481d05dd864033e25025d8dc
Millatinaa <millatinarosyadaaa21@gmail.com>
2021-10-04 08:36:04 +0700

    Upload awal project

---
+++ application/views/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public funtion index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Millatina Rosyada
+        Saya tingal di bumi lestari
+        dan hobby saya makan sambil nonton live tiktok";
+    }
+}
