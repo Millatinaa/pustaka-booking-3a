7992ea3a86fd6faa74fe4338efe2f27503cfdf06
Millatinaa <millatinarosyadaaa21@gmail.com>
2021-10-04 09:55:38 +0700

    test

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,8 +3,8 @@ class contoh1 extends CI_Controller
 { 
     public function index() 
     { echo "<h1>Perkenalkan</h1>"; 
-        echo"Nama saya Imam Nawawi Saya 
-        tingga di daerah Ciputat olah raga yang saya 
+        echo"Nama saya Millatina Rosyada Saya 
+        tinggal di daerah Bekasi olah raga yang saya 
         sukai adalah Bulutangkis"; 
     } 
 }
