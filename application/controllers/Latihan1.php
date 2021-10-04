8f9b446b5fc9e5a05633ad8d28b74e1945c1afbf
Millatinaa <millatinarosyadaaa21@gmail.com>
2021-10-04 08:39:41 +0700

    pustaka - booking

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,8 @@
+   <?php
+   class Latihan1 extends CI_Controller
+   {
+   public function index()
+   		{
+   			echo "<b><h2>Selamat Datang.. selamat belajar Web Programing</h2><b>";
+   		}
+  }
